cb0a016e4ff54b77b24121814e3ae0b99cfc9bb7
Sasmita <mail2sasmitamandal@gmail.com>
2021-11-10 10:08:47 +0530

    login modified

--- src/main/java/com/hutech/payrollapp/api/controller/LoginController.java
+++ src/main/java/com/hutech/payrollapp/api/controller/LoginController.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +24,7 @@ import com.hutech.payrollapp.api.serviceImpl.LoginService;
 
 @RestController
 @RequestMapping
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class LoginController {
 
 	
